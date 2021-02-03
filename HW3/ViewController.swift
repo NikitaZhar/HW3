8b3ce1858e57fa82641daf4637126a392729ef8f
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-02-03 11:17:46 +0100

    Second screen with elements

--- HW3/ViewController.swift
+++ HW3/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func forgotMessageButton(_ sender: UIButton) {
+        switch sender.tag {
+        case 0: forgotMessage(with: "No Problem!", and: "Your username is User")
+        case 1: forgotMessage(with: "No Problem!", and: "Your password is Password")
+        default: break
+        }
+    }
+    
+}
 
+extension ViewController {
+    private func forgotMessage(with alertTitle: String, and alertMessage: String) {
+        let alertView = UIAlertController(
+            title: alertTitle,
+            message: alertMessage,
+            preferredStyle: .alert
+        )
+        
+        let okAlertButton = UIAlertAction(title: "OK", style: .default)
+//        {
+//            _ in self.textField.text = ""
+//        }
+        alertView.addAction(okAlertButton)
+        present(alertView, animated: true)
+
+    }
 }
 
